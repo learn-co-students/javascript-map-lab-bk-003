0ef9e2f5131d1b2bc510297999d6485dfea4325e
Omrika <omrikafolkes@gmail.com>
2017-02-03 19:54:25 -0500

    cool

--- index.js
+++ index.js
@@ -21,3 +21,15 @@ const zebraStripes = [
   { width: 0.59, color: null },
   { width: 7.31, color: null },
 ];
+
+const sortedRobots = robots.map(function(robot){
+  return Object.assign({}, robot, 
+    knownDecepticons.includes(robot.name) ? { alliance: 'decepticon'} : { alliance: 'autobot' }
+  );
+})
+
+const coloredZebraStripes = zebraStripes.map(function(stripes, index){
+  return Object.assign({}, stripes, 
+    index % 2 === 0 ? { color: 'black' } : { color: 'white' }
+  );
+})
